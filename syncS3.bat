0d5ecd6f60d0abe06d2d08822571c149cb79c5c6
froster2006 <huaningnie@gmail.com>
2026-06-01 17:13:50 -0400

    update sync to 60s loop

--- syncS3.bat
+++ syncS3.bat
@@ -1,2 +1,14 @@
+@echo off
+title WCCA update Loop
+
+:loop
+
+echo [%date% %time%] Running jobs...
+
 aws s3 cp .\matches.json s3://wcca-pickleball-club --profile sls
 aws s3 cp .\ranking.json s3://wcca-pickleball-club --profile sls
+
+echo [%date% %time%] Done. Waiting 60 seconds...
+timeout /t 60 /nobreak >nul
+
+goto loop
